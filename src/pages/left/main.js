c9b7a060fe9f68d4c36aa0e99c63975e241f3e53
牛星星 <nxx123456>
2021-02-01 20:53:46 +0800

    提交

--- src/pages/left/main.js
+++ src/pages/left/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './index.vue'
 import router from './router'
+import * as echarts from 'echarts';
 // Vue.prototype.$http = service
 // Vue.prototype.utils = utils;
 Vue.config.productionTip = false
